a3affece66fa6d7b3639cdc7ee9d0fcd5e37ae44
barikhushi <khushibari2421@gmail.com>
2025-07-12 11:10:43 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,23 @@
+# Simple calculator in Python
+
+# Get inputs from the user
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation based on the operator
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
